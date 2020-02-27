42224f838bf21961d5710e67fbdebbc5ebda2f09
samiur.rahman <samiur.rahman@summitcommunications.net>
2020-02-27 17:25:37 +0600

    virtual added for relations

--- OAS.Core.Entity/Employee.cs
+++ OAS.Core.Entity/Employee.cs
@@ -8,6 +8,6 @@ namespace OAS.Core.Entity
     public class Employee : BaseModel
     {
         public int Name { get; set; }
-        public Department Department { get; set; }
+        public virtual Department Department { get; set; }
     }
 }
