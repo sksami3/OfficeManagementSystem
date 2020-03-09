4a96788f4395c7a3321e9a20059362eb53543c9f
sksami3 <sksami4456@gmail.com>
2020-03-09 23:27:40 +0600

    get all service added

--- Angular/src/app/Shared/Api/department.service.ts
+++ Angular/src/app/Shared/Api/department.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+// import { Observable } from 'rxjs/Observable';
+import { Department } from 'src/app/Shared/Models/Department'
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DepartmentService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+
+  public API = 'http://localhost:33333/api';
+  public DepartmetAPI = `${this.API}/Departments`;
+
+  getAll() : Observable<Department[]>{
+    return this.http.get<Department[]>(`${this.DepartmetAPI}`);
+  }
 }
