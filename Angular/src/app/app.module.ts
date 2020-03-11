3883617a298589fa853f9253df4e759e0a525c6c
samiur.rahman <samiur.rahman@summitcommunications.net>
2020-03-11 17:22:08 +0600

    Department List restored

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DepartmentListComponent } from './Department/department-list/department-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DepartmentListComponent
   ],
   imports: [
     BrowserModule
