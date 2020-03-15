2042a3de3f3efb5d9a183ff285bfd43f9489b233
samiur.rahman <samiur.rahman@summitcommunications.net>
2020-03-15 17:20:47 +0600

    edit component added

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { DepartmentListComponent } from './Department/department-list/department-list.component';
 import { DepartmentService } from './Shared/Api/department.service';
+import { DepartmentEditComponent } from './Department/department-edit/department-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DepartmentListComponent
+    DepartmentListComponent,
+    DepartmentEditComponent
   ],
   imports: [
     BrowserModule,
