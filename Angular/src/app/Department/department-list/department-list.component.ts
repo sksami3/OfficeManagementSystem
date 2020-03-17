ab4a1f86ca222ac5296c866e92de0acb7fdb8012
sksami3 <sksami4456@gmail.com>
2020-03-17 23:35:26 +0600

    button working

--- Angular/src/app/Department/department-list/department-list.component.ts
+++ Angular/src/app/Department/department-list/department-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DepartmentService } from 'src/app/Shared/Api/department.service';
 import { Department } from 'src/app/Shared/Models/Department';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-department-list',
@@ -11,12 +12,17 @@ export class DepartmentListComponent implements OnInit {
 
   Title = "Department List";
   departmetnList: any= Array<Department>();
-  constructor(private departmentServive : DepartmentService) {
+  constructor(private departmentServive : DepartmentService, private router: Router) {
       console.log("In Constructor");
       //this.departmetnList = departmentServive.getAll().subscribe(res=>this.departmetnList=res);
    }
 
    columnsToDisplay = ['id','name','actions'];
+
+   editDepartment(){
+     console.log("Edit department");
+      this.router.navigate(['']);
+    }
 
   ngOnInit(): void {
     console.log("ngOnInit");
